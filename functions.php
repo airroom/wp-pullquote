airroom/wp-pullquote

commit da000966975b44e0ad9eafcb440d0b20c05c3f9f

remove faux locale support

includes/core.php
@@ -20,7 +20,6 @@ function pullquote_enqueue_scripts() {
         wp_enqueue_script( 'jquery' );
         wp_enqueue_script( 'realtidbitsPushquote', pullquote_plugin_url( 'js/pullquote.js' ),
             array('jquery'), PULLQUOTE_VER, false); 
-        wp_localize_script( 'realtidbitsPushquote', 'PushquoteAjax', array( 'show_credits' => (is_array($realtidbitsPushquote) ? $realtidbitsPushquote['show_credits'] : 0 ) ) );
     }
 }
 

pullquote.php
@@ -21,9 +21,6 @@ if (!defined('REALPUSHQUOTE_PLUGIN_BASENAME')) {
     define('REALPUSHQUOTE_PLUGIN_BASENAME', plugin_basename(__FILE__));
 }
 
-// Create Text Domain For Translations
-load_plugin_textdomain('realtidbitsPushquote', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');
-
 function checkMU_install_realtidbitsPushquote($network_wide) {
     global $wpdb;
     if ( $network_wide ) {

commit a0c99c488dbc86670436432d7e593e199774c0c4

slim down PHP contents
- tidy PHP files
- fix JS issue introduced in da000966975b44e0ad9eafcb440d0b20c05c3f9f

functions.php
@@ -37,4 +37,3 @@ function add_pullquote_tinymce_plugin($plugin_array) {
  
 // init process for button control
 add_action('init', 'pullquote_addbuttons');
-?>

includes/core.php
@@ -1,6 +1,7 @@
-<?php 
-
-// Shortcode
+<?php
+add_shortcode('pullquote', 'pullquote_simple_shortcode');
+add_action('init', 'pullquote_enqueue_scripts');
+add_action('wp', 'pullquote_enqueue_styles');
 
 function pullquote_simple_shortcode($atts, $content) {
     extract(shortcode_atts(array(
@@ -10,29 +11,18 @@ function pullquote_simple_shortcode($atts, $content) {
     
     return "<span class='pullquote-source' data-float='".$position."' data-hidden='".$hidden."'>".$content."</span>";
 }
-add_shortcode('pullquote', 'pullquote_simple_shortcode');
-
-// Javascript Files
 
 function pullquote_enqueue_scripts() {
-    global $realtidbitsPushquote;
+    global $pullquoteOptions;
     if ( !is_admin() ){ 
-        wp_enqueue_script( 'jquery' );
-        wp_enqueue_script( 'realtidbitsPushquote', pullquote_plugin_url( 'js/pullquote.js' ),
+        wp_enqueue_script('jquery' );
+        wp_enqueue_script('pullquote', pullquote_plugin_url('js/pullquote.js'),
             array('jquery'), PULLQUOTE_VER, false); 
     }
 }
 
-add_action( 'init', 'pullquote_enqueue_scripts' );
-
-// Styles 
-
 function pullquote_enqueue_styles() {
-  global $post, $realtidbitsPushquote, $wp_registered_widgets, $wp_widget_factory;
-  
-  wp_enqueue_style( 'realtidbitsPushquote_headcss', pullquote_plugin_url( 'css/pullquote.css' ),
+  global $pullquoteOptions;
+  wp_enqueue_style('pullquote_headcss', pullquote_plugin_url('css/pullquote.css'),
     false, PULLQUOTE_VER, 'all');
 }
-add_action( 'wp', 'pullquote_enqueue_styles' );
-
-?>

js/pullquote.js
@@ -65,14 +65,6 @@ jQuery(document).ready(function() {
                    
                     return false;
                   });
-        if(PushquoteAjax.show_credits == 1) {
-          jQuery(pullquote)
-            .append(
-              jQuery('<div />')
-                .addClass('pullquote_copyright')
-                .html('Pullquote Plugin by <a href="http://www.pushquote.net" target="_blank">PushQuote</a>')
-            );
-        }
         jQuery(pullquote)
           .append(
             jQuery('<div />')

pullquote.php
@@ -9,87 +9,77 @@
  * License: GPL2
  */
 
-//on activation
-//defined global variables and constants here
+// TODO currently, $pullquoteOptions contains 'active' only but never uses it
 
-global $realtidbitsPushquote;
-$realtidbitsPushquote = get_option('realtidbitsPushquote_options');
+define("PULLQUOTE_VER", "20131025", false);
 
-define("PULLQUOTE_VER", "1.0", false);
-
-if (!defined('REALPUSHQUOTE_PLUGIN_BASENAME')) {
-    define('REALPUSHQUOTE_PLUGIN_BASENAME', plugin_basename(__FILE__));
+if (!defined('PULLQUOTE_PLUGIN_BASENAME')) {
+    define('PULLQUOTE_PLUGIN_BASENAME', plugin_basename(__FILE__));
 }
 
-function checkMU_install_realtidbitsPushquote($network_wide) {
-    global $wpdb;
-    if ( $network_wide ) {
-        $blog_list = get_blog_list( 0, 'all' );
-        foreach ($blog_list as $blog) {
-            switch_to_blog($blog['blog_id']);
-            install_realtidbitsPushquote();
-        }
-        switch_to_blog($wpdb->blogid);
-    } else {
-        install_realtidbitsPushquote();
-    }
-}
+global $pullquoteOptions;
+$pullquoteOptions = get_option('pullquote_options');
 
-function install_realtidbitsPushquote() {
+require_once(dirname(__FILE__) . '/functions.php');
+require_once(dirname(__FILE__) . '/includes/core.php');
+
+
+function install_pullquote() {
     $default_settings = array(
-        'active' => false,
-        'show_credits' => false
+        'active' => false
     );
-    
-    $realtidbitsPushquote = array();
-    
-    foreach ($default_settings as $key=>$value) {
-      if (!isset($realtidbitsPushquote[$key])) {
-         $realtidbitsPushquote[$key] = $value;
-      }
-    }
-    
-    delete_option('realtidbitsPushquote_options');      
-    update_option('realtidbitsPushquote_options', $realtidbitsPushquote);
+        
+    delete_option('pullquote_options');      
+    update_option('pullquote_options', $default_settings);
 }
-register_activation_hook( __FILE__, 'checkMU_install_realtidbitsPushquote' );
 
-/* Uninstall */
-function checkMU_uninstall_realtidbitsPushquote($network_wide) {
+function uninstall_pullquote() {
+    global $wpdb;
+    delete_option('pullquote_options'); 
+}
+
+/*
+ * MU Options
+ */
+register_activation_hook( __FILE__, 'checkMU_install_pullquote' );
+register_uninstall_hook( __FILE__, 'checkMU_uninstall_pullquote' );
+add_action('wpmu_new_blog', 'newBlog_pullquote', 10, 6);
+
+function checkMU_install_pullquote($network_wide) {
     global $wpdb;
     if ( $network_wide ) {
         $blog_list = get_blog_list( 0, 'all' );
         foreach ($blog_list as $blog) {
             switch_to_blog($blog['blog_id']);
-            uninstall_realtidbitsPushquote();
+            install_pullquote();
         }
         switch_to_blog($wpdb->blogid);
     } else {
-        uninstall_realtidbitsPushquote();
+        install_pullquote();
     }
 }
 
-function uninstall_realtidbitsPushquote() {
+function checkMU_uninstall_pullquote($network_wide) {
     global $wpdb;
-    delete_option('realtidbitsPushquote_options'); 
+    if ( $network_wide ) {
+        $blog_list = get_blog_list( 0, 'all' );
+        foreach ($blog_list as $blog) {
+            switch_to_blog($blog['blog_id']);
+            uninstall_pullquote();
+        }
+        switch_to_blog($wpdb->blogid);
+    } else {
+        uninstall_pullquote();
+    }
 }
-register_uninstall_hook( __FILE__, 'checkMU_uninstall_realtidbitsPushquote' );
 
-/* Add new Blog */
-
-add_action( 'wpmu_new_blog', 'newBlog_realtidbitsPushquote', 10, 6);         
- 
-function newBlog_realtidbitsPushquote($blog_id, $user_id, $domain, $path, $site_id, $meta ) {
+function newBlog_pullquote($blog_id, $user_id, $domain, $path, $site_id, $meta ) {
     global $wpdb;
  
     if (is_plugin_active_for_network('pullquote/pullquote.php')) {
         $old_blog = $wpdb->blogid;
         switch_to_blog($blog_id);
-        install_realtidbitsPushquote();
+        install_pullquote();
         switch_to_blog($old_blog);
     }
 }
-
-require_once (dirname (__FILE__) . '/functions.php');
-require_once (dirname (__FILE__) . '/includes/core.php');
-?>